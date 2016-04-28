74b150e10d1ac51832fd9308da962db3ab78fde0
pokpongt <pokpongt@hotmail.com>
2016-04-28 00:37:58 -0400

    validate funame field ,must have , no more than 50 character

--- AirAlien/app/models/user.rb
+++ AirAlien/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validates :fullname, presence: true, lenght: {maximum: 50} 
+  
 end
